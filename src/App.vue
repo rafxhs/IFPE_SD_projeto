c142549e09b2c930a7684d68a08af9e0f7628b87
Alan José <alanjsdelima@gmail.com>
2025-06-21 21:23:05 -0300

    Deixando tamanho do container de resultado automático, adicionando selects para as linguagens base e alvo.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 const userInput = ref('')
+const baseLanguage = ref('pt_BR')
+const targetLanguage = ref('en_US')
 
 function clearInput() {
   userInput.value = '' 
@@ -13,11 +15,20 @@ function clearInput() {
   <div class="flex flex-col h-screen w-screen items-start gap-4 bg-white p-10">
     <h1>Tradutor SD</h1>
     <div class="flex flex-row space-x-4">   
-      <div class="relative">
+      <div class="relative rounded-lg border border-gray-200">
+
+         <select v-model="baseLanguage" class="absolute top-2 left-2 rounded-full text-gray-800 py-2 px-4 text-sm bg-white border border-gray-200" id="baseLanguageSelect">
+          <option value="pt_BR" selected>Português</option>
+          <option value="en_US">Inglês</option>
+          <option value="es_ES">Espanhol</option>
+          <option value="fr_FR">Francês</option>
+        </select>
+
+        <br>
 
         <textarea
         v-model="userInput"
-        class="w-[623px] h-[164px] p-4 pr-12 rounded-lg border border-gray-200 p-4 bg-white text-gray-800 resize-none focus:outline-none focus:ring-0"
+        class="w-[623px] h-[164px] mt-2 p-4 pr-12 p-4 bg-white text-gray-800 resize-none focus:outline-none focus:ring-0"
         />
       
         <button
@@ -32,8 +43,17 @@ function clearInput() {
         </button>
   </div>
 
-  <div class="w-[623px] h-[164px] p-4 pr-12 rounded-lg p-4 bg-gray-200 shadow break-words whitespace-normal">
-    {{ userInput || 'Tradução' }}
+  <div class="w-[623px] h-[auto] p-4 pr-12 rounded-lg p-4 bg-gray-200 shadow break-words whitespace-normal">
+    <select v-model="targetLanguage" class="top-2 left-2 rounded-full text-gray-800 py-2 px-4 text-sm bg-white border border-gray-200" id="targetLanguageSelect">
+          <option value="pt_BR" selected>Português</option>
+          <option value="en_US">Inglês</option>
+          <option value="es_ES">Espanhol</option>
+          <option value="fr_FR">Francês</option>
+        </select>
+        <br>
+        <p class="mt-2">
+          {{ userInput || 'Tradução' }}
+        </p>
   </div>
 
     </div>
